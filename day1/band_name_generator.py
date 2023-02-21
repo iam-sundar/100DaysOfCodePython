1a1083a6baccb9e7122f9b6a16fc6bbb3596016c
suntha <sundar.thambidurai@hidglobal.com>
2023-02-21 23:48:02 +0530

    Day 1: #100DaysOfCode #Python Band Name Generaor simple program.

---
+++ day1/band_name_generator.py
@@ -0,0 +1,5 @@
+print('Welcome to the Band Name Generator.')
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What is the name of a pet?\n")
+band_name = city_name + " " + pet_name
+print("Your band name could be " + band_name)
